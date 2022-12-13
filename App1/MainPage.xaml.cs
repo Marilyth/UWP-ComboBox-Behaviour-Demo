0c2ad275c231ae29cb36e1333ed72f445779b298
May <baermay98@gmail.com>
2022-12-13 10:12:23 +0100

    Update MainPage.xaml.cs

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -1,33 +1,58 @@
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace App1
 {
-    public sealed partial class MainPage : Page, INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-        public List<string> Items { get; set; } = new List<string>() { "Test1", "Test2" };
-        public int SelectedIndex { get; set; } = 1;
+    public sealed partial class MainPage : Page 
+    { 
+        public ObservableCollection<Test> Items { get; set; } = new ObservableCollection<Test>() { new Test(), new Test() };
 
         public MainPage()
         {
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            // Replace Items, keeping everything default.
-            Items = new List<string>() { "Test1", "Test2" };
-            SelectedIndex = 1;
+            // Bug only triggers when items are removed.
+            // Remove this line and it doesn't happen anymore.
+            Items.RemoveAt(0);
 
-            // Sets internal SelectedIndex to -1 if it wasn't -1.
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Items)));
+            // It happens also after a delay. But the pattern changes slightly.
+            //await Task.Delay(200);
 
-            // Changing SelectedIndex here instead would avoid this bug.
-            // SelectedIndex = 1;
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedIndex)));
+            // The first n new items after n removals on an ObservableCollection have a chance to be set to null.
+            // When and how often it happens is correlated to the amount of items in the collection at the time of removal.
+            Items.Add(new Test());
+
+            // It follows a pattern, e.g. 1 removed and 1 inserted per step:
+            // Test1, null, null, null, Test1, Test1, Test1, null, null, null, ...
+        }
+    }
+
+    public class Test : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public List<string> Options { get; set; } = new List<string>() { "Test1", "Test2" };
+
+        private string _SelectedItem = "Test1";
+        public string SelectedItem { 
+            get { 
+                return _SelectedItem; 
+            } 
+            set {
+                if (_SelectedItem != value)
+                {
+                    _SelectedItem = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedItem)));
+                }
+            } 
         }
     }
 }
